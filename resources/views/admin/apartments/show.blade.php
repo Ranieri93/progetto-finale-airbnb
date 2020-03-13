48fcba399cb1606502ec4031d08884abb969f7cd
Fabienne <54371769+fabiennefortunio@users.noreply.github.com>
2020-03-13 21:07:11 +0100

    show.blade.php
    SCSS partials created.
    
    currently working on the 'show' view.

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.admin')
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-6 offset-3">
-                <div class="card" style="width: 18rem;">
+    {{-- image section --}}
+    <div class="card apt-img">
+        <img src="{{$apartment->cover_image ? asset('storage/' . $apartment->cover_image) : asset('storage/uploads/404.png') }}"
+        alt="{{$apartment->sommary_title}}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text apt-title"> {{$apartment->sommary_title}}</p>
+        </div>
+    </div>
+
+    <div class="container" style="width:100%">
+        <div class="d-flex" style="">
+            <div class="p-2 flex-grow-* bd-highlight">
+                <p>Descrizione
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos minus debitis officiis delectus, reprehenderit voluptas! Architecto nam laborum ab voluptatem, repudiandae, ratione consequuntur ullam illum, consequatur accusantium aspernatur assumenda ipsam.
+                </p>
+            <p>{{ $apartment->description }}</p>
+            </div>
+            <div class="p-2 flex-grow-* bd-highlight" style="width:40vw;">
+                <div class="card">
                     <div class="card-body">
-                        <h4 class="card-title"> {{$apartment->sommary_title}}</h4>
+                        {{-- <h4 class="card-title"> {{$apartment->sommary_title}}</h4> --}}
                         <a class="btn btn-dark" href="{{route('admin.apartments.index')}}">Torna indietro</a>
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item">Slug: <br> {{ $apartment->slug }}</li>
-                            <li class="list-group-item">Descrizione: <br> {{ $apartment->description }}</li>
+                            {{-- <li class="list-group-item">Slug: <br> {{ $apartment->slug }}</li> --}}
+                            {{-- <li class="list-group-item">Descrizione: <br> {{ $apartment->description }}</li> --}}
                             <li class="list-group-item">Numero Stanze: <br> {{ $apartment->room_number }}</li>
                             <li class="list-group-item">Numero Ospiti: <br> {{ $apartment->guest_number }}</li>
                             <li class="list-group-item">Numero Bagni: <br> {{ $apartment->wc_number}}</li>
@@ -20,16 +35,21 @@
                                 @empty
                                     -
                                 @endforelse
-                            </li>
+                            {{-- </li>
                             <li class="list-group-item"><img
                                     src="{{$apartment->cover_image ? asset('storage/' . $apartment->cover_image) : asset('storage/uploads/404.png') }}"
                                                              alt="{{$apartment->sommary_title}}">
-                            </li>
+                            </li> --}}
 
                         </ul>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
+
+    {{-- MAP SECTION --}}
+
+
 @endsection
