e2fe874e01b98b98939c5ed6c106b76468b8cd62
Fabienne <54371769+fabiennefortunio@users.noreply.github.com>
2020-03-14 11:18:43 +0100

    prova
    prova

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-    //    $this->call(ApartmentsTableSeeder::class);
-        $this->call(AdminTableSeeder::class);
+       $this->call(ApartmentsTableSeeder::class);
+        // $this->call(AdminTableSeeder::class);
 
         // $this->call(ServiceTableSeeder::class);
     }
