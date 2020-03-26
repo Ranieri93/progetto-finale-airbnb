abac4ea46824ebd4433dc9433c3f83e9dd500472
Ranieri93 <ranieri.innocenti@gmil.com>
2020-03-26 15:24:59 +0100

    Fixed Create

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -1,31 +1,5 @@
 @extends('layouts.admin')
 @section('content')
-<script>
-    function ConfermaOperazione() {
-        var confirm = window.confirm("Il tuo appartamento è stato aggiunto con successo ! clicca 'OK' per proseguire");
-        return confirm;
-    }
-</script>
-<script>
-    function validateForm() {
-        var a = document.forms["Form"]["form1"].value;
-        var b = document.forms["Form"]["form2"].value;
-        var c = document.forms["Form"]["form3"].value;
-        var d = document.forms["Form"]["form4"].value;
-        var e = document.forms["Form"]["form5"].value;
-        var f = document.forms["Form"]["form6"].value;
-        var g = document.forms["Form"]["form7"].value;
-        if (a == "" || a == null || b == "" || b == null || isNaN(c) ||
-            c == "" || c == null || d == "" || d == null || isNaN(d) ||
-            e == "" || e == null || f == "" || f == null || isNaN(e) ||
-            g == "" || g == null || isNaN(f)) {
-            alert("Controlla i tuoi dati");
-            return false;
-        } else {
-            ConfermaOperazione();
-        }
-    }
-</script>
 <div class="container mt-5 mb-5">
     <div class="row">
         <div class="col-6 offset-3">
@@ -43,27 +17,27 @@
                 @csrf
                 <div class="form-group">
                     <label for="sommary_title">Titolo Descrittivo</label>
-                    <input type="text" class="form-control" name="form1" name="sommary_title" id="sommary_title" placeholder="Scrivi qui il titolo descrittivo" value="{{old('sommary_title')}}">
+                    <input type="text" class="form-control"  name="sommary_title" id="sommary_title" placeholder="Scrivi qui il titolo descrittivo" value="{{old('sommary_title')}}">
                 </div>
                 <div class="form-group">
                     <label for="description">Testo Articolo</label>
-                    <textarea name="form2" name="description" id="description" class="description" cols="80">{{old('description')}}</textarea>
+                    <textarea name="description" id="description" class="description" cols="80">{{old('description')}}</textarea>
                 </div>
                 <div class="form-group">
                     <label for="room_number">Numero Stanze</label>
-                    <input type="text" name="form3" class="form-control" name="room_number" id="room_number" placeholder="Scrivi qui il numero delle stanze" value="{{old('room_number')}}">
+                    <input type="text" class="form-control" name="room_number" id="room_number" placeholder="Scrivi qui il numero delle stanze" value="{{old('room_number')}}">
                 </div>
                 <div class="form-group">
                     <label for="guest_number">Numero Ospiti</label>
-                    <input type="text" name="form4" class="form-control" name="guest_number" id="guest_number" placeholder="Scrivi qui il numero degli ospiti" value="{{old('guest_number')}}">
+                    <input type="text" class="form-control" name="guest_number" id="guest_number" placeholder="Scrivi qui il numero degli ospiti" value="{{old('guest_number')}}">
                 </div>
                 <div class="form-group">
                     <label for="wc_number">Numero Bagni</label>
-                    <input type="text" name="form5" class="form-control" name="wc_number" id="wc_number" placeholder="Scrivi qui il numero dei bagni" value="{{old('wc_number')}}">
+                    <input type="text" class="form-control" name="wc_number" id="wc_number" placeholder="Scrivi qui il numero dei bagni" value="{{old('wc_number')}}">
                 </div>
                 <div class="form-group">
                     <label for="square_meters">Metri Quadrati</label>
-                    <input type="text" name="form6" class="form-control" name="square_meters" id="square_meters" placeholder="Scrivi qui la metratura" value="{{old('square_meters')}}">
+                    <input type="text" class="form-control" name="square_meters" id="square_meters" placeholder="Scrivi qui la metratura" value="{{old('square_meters')}}">
                 </div>
 
                 @if($services->count())
@@ -79,7 +53,7 @@
 
                 <div class="form-group">
                     <label for="address">Inserisci la città e l'indirizzo</label>
-                    <input type="text" name="form7" class="form-control" name="address" id="address" placeholder="Scrivi qui l'indirizzo" value="Scrivi qui l'indirizzo">
+                    <input type="text" class="form-control" name="address" id="address" placeholder="Scrivi qui l'indirizzo" value="Scrivi qui l'indirizzo">
                 </div>
 
                 <div class="form-group">
